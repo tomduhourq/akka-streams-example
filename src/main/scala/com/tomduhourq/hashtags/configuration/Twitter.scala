94aaf56b22a285e358e35552eb1010c772cd7752
tomduhourq <t.duhourq@gmail.com>
2016-12-02 00:46:27 -0300

    Initial commit

--- src/main/scala/com/tomduhourq/hashtags/configuration/Twitter.scala
+++ src/main/scala/com/tomduhourq/hashtags/configuration/Twitter.scala
@@ -7,10 +7,10 @@ trait Twitter {
   val cb = new ConfigurationBuilder()
 
   cb.setDebugEnabled(true)
-    .setOAuthConsumerKey("d9DZgRHpYc0SeWR2tNnMi2RVj")
-    .setOAuthConsumerSecret("XBrntcAm1mMK14ymVJEAEfgb01ThYVh6oYVQl8uPo9Wb9e6t8q")
-    .setOAuthAccessToken("2199571344-t5dqEdAX60Konm7A85iEK8xHoc5YSoSZg4uOgFK")
-    .setOAuthAccessTokenSecret("5oPE7dAOTvopcrmsy0BKAIkiYfyxUvVazzK0oJYtLplqr")
+    .setOAuthConsumerKey("xxxxxxx")
+    .setOAuthConsumerSecret("xxxxxxx")
+    .setOAuthAccessToken("xxxxxxx")
+    .setOAuthAccessTokenSecret("xxxxxxx")
 
   private val twitterConfBuilder = cb.build()
   val twitter = new TwitterFactory(twitterConfBuilder).getInstance()
